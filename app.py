ce0b778bac084760c6bf6cc7fc2c52c06384bd3c
Sambit Chakraborty <82374370+ismiit@users.noreply.github.com>
2022-03-01 15:37:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import tensorflow as tf
+import keras
 import time
 from sklearn.preprocessing import StandardScaler
 
